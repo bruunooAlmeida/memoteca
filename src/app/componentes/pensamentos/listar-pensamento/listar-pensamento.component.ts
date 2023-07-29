f16d0731890a553b88bdfca6976ed7944318fffb
bruunooAlmeida <almeidabrunoti@gmail.com>
2023-07-29 18:40:51 -0300

    Finalizando Aula 3

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -5,18 +5,28 @@ import { Pensamento } from '../pensamento';
 @Component({
   selector: 'app-listar-pensamento',
   templateUrl: './listar-pensamento.component.html',
-  styleUrls: ['./listar-pensamento.component.css']
+  styleUrls: ['./listar-pensamento.component.css'],
 })
 export class ListarPensamentoComponent implements OnInit {
-
   listaPensamentos: Pensamento[] = [];
-
-  constructor(private service: PensamentoService) { }
+  paginaAtual: number = 1;
+  haMaisPensamentos: boolean = true;
+  constructor(private service: PensamentoService) {}
 
   ngOnInit(): void {
-    this.service.listar().subscribe((listaPensamentos) => {
-      this.listaPensamentos = listaPensamentos
+    this.service.listar(this.paginaAtual).subscribe((listaPensamentos) => {
+      this.listaPensamentos = listaPensamentos;
+    });
+  }
+
+  carregarMaisPensamentos(){
+    this.service.listar(++this.paginaAtual)
+    .subscribe(listaPensamentos => {
+        this.listaPensamentos.push(...listaPensamentos);
+
+        if(!listaPensamentos.length){
+          this.haMaisPensamentos = false;
+        }
     })
   }
 }
-
